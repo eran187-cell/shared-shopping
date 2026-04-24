cedc2e2920be0246349e927ab5cb6136cd56104c
eran187-cell <eran187@gmail.com>
2026-04-24 09:20:47 +0300

    New green logo, splash screen on startup, green color theme throughout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sl-v1';
+const CACHE='sl-v3';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
