5a332ac0a99735bd8422049cab58a71b28996a7e
eran187-cell <eran187@users.noreply.github.com>
2026-05-24 15:07:21 +0300

    Deploy from test → ss-v9 (app v1.0)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE='ss-v8';
+﻿const CACHE='ss-v9';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
